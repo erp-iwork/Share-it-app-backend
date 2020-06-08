6be59b5cddcf0c30c49ae577553391bdcdd60a89
kidusss <kidussolomon2004@gmail.com>
2020-06-08 15:27:26 +0300

    unit test for our custom user model

---
+++ main/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_create_user_with_email_success(self):
+        """Test creating a new user with email is successful"""
+        email = "test@gmail.com"
+        password = 1234567
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEquals(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_email_is_normalized(self):
+        """Test the email field is normalized"""
+        email = "test@EXAMPLE.com"
+        password = 1234567
+        user = get_user_model().objects.create_user(email=email, password=password)
+
+        self.assertEquals(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """"Test creating user with invalid email raises error"""
+        with self.assertRaises(ValueError):
+            password = 1234567
+            user = get_user_model().objects.create_user(None, password=password)
