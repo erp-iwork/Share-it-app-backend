fdf2fb2986e2dd8f95399ea581d6534f9887d9ec
kidusss <kidussolomon2004@gmail.com>
2020-06-09 15:26:09 +0300

    admin customization for custom user

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext as _
 
-# Register your models here.
+from main import models
+
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ["id"]
+    list_display = ["email", "first_name"]
+    list_filter = ("updated_at",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal Info"), {"fields": ("first_name", "last_name", "gender")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (_("Important_dates"), {"fields": ("last_login",)}),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
+admin.site.site_header = "Share it admin page"
---
+++ main/tests/test_admin.py
@@ -0,0 +1,35 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        """Setup an admin user and a dummy use for test"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@gmail.com", password="admin"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="user@gmail.com",
+            password="user",
+            first_name="user",
+            last_name="lastname",
+            gender="male",
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on user page"""
+        url = reverse("admin:main_user_changelist")
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.first_name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse("admin:main_user_change", args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEquals(res.status_code, 200)
