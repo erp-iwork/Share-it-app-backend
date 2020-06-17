6dc6da3225a129d0e0f4c8dcf838a69003150af7
kidusss <kidussolomon2004@gmail.com>
2020-06-17 20:10:33 +0300

    staticfile change

--- src/settings.py
+++ src/settings.py
@@ -132,7 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 os.makedirs(STATIC_TMP, exist_ok=True)
 os.makedirs(STATIC_ROOT, exist_ok=True)
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = ["https://localhost:3000"]
