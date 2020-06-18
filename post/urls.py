81a7c422e707c5dc07da09b38a79e24d4f5fa057
kidusss <kidussolomon2004@gmail.com>
2020-06-18 23:30:45 +0300

    items filter by category and items posted by loged in user

--- post/urls.py
+++ post/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path, include
-from .views import ItemListAdd, ItemRUD
+from post import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = "post"
 
 urlpatterns = [
-    path("item/", ItemListAdd.as_view(), name="item_add_list"),
-    path("item/<str:itemId>/", ItemRUD.as_view(), name="item_rud"),
+    path("item/", views.ItemListAdd.as_view(), name="item_add_list"),
+    path("item/<str:itemId>/", views.ItemRUD.as_view(), name="item_rud"),
+    path("my-items/", views.UserItemList.as_view(), name="my_items"),
+    path("items", views.ItemFilterView.as_view(), name="category_items"),
+    path("item-property", views.PropertyFilterView.as_view(), name="properties_items"),
 ]
 # Adding static files
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- post/views.py
+++ post/views.py
@@ -3,13 +3,17 @@ from rest_framework.response import Response
 from utilities.exception_handler import CustomValidation
 from utilities.permission import IsAuthenticatedOrReadOnly
 
-from main.models import ItemImageModel, ItemModel
-from .serializers import ItemSerializer
+from main.models import ItemImageModel, ItemModel, Category
+from .serializers import ItemSerializer, CategorySerializer
+from django_filters import rest_framework as filters
+import json
 
 
 # Add or post item api view with multiple images
 class ItemListAdd(generics.ListCreateAPIView):
-    """Allow to post item only authenticated user"""
+    """
+    Allow to post item only authenticated user
+    """
 
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
@@ -26,7 +30,9 @@ class ItemListAdd(generics.ListCreateAPIView):
 
 
 class ItemRUD(generics.RetrieveUpdateDestroyAPIView):
-    """View that can handle item get, update and delete"""
+    """
+    View that can handle item get, update and delete
+    """
 
     permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = ItemSerializer
@@ -41,3 +47,53 @@ class ItemRUD(generics.RetrieveUpdateDestroyAPIView):
 
     def delete(self, request, itemId=None):
         return self.destroy(request, itemId)  # send custom deletion success message
+
+
+class UserItemList(generics.ListAPIView):
+    serializer_class = ItemSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the items posted
+        for the currently authenticated user.
+        """
+        user = self.request.user
+        return ItemModel.objects.filter(owner=user)
+
+
+class ItemFilter(filters.FilterSet):
+    min_price = filters.NumberFilter(field_name="price", lookup_expr="gte")
+    max_price = filters.NumberFilter(field_name="price", lookup_expr="lte")
+
+    class Meta:
+        model = ItemModel
+        fields = ["category", "min_price", "max_price", "condition"]
+
+
+class ItemFilterView(generics.ListAPIView):
+    """
+    Return items in specific category
+    """
+
+    queryset = ItemModel.objects.all()
+    serializer_class = ItemSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = ItemFilter
+
+
+class PropertyFilterView(generics.ListAPIView):
+    """
+    Return items filtered by property in a given category
+    """
+
+    serializer_class = ItemSerializer
+    queryset = ItemModel.objects.all()
+
+    def get_queryset(self):
+        category = self.request.query_params.get("category", None)
+        property = self.request.query_params.get("property", None)
+        property_dict = json.loads(property)
+        return ItemModel.objects.filter(
+            category=category, properties__contains=property_dict
+        )
