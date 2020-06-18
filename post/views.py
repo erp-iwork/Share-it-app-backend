c6971e23048ec143a99e1dbfa3e2724bce42030f
kidusss <kidussolomon2004@gmail.com>
2020-06-18 14:41:14 +0300

    posting functionality

---
+++ post/views.py
@@ -0,0 +1,43 @@
+from rest_framework import authentication, generics, permissions, status
+from rest_framework.response import Response
+from utilities.exception_handler import CustomValidation
+from utilities.permission import IsAuthenticatedOrReadOnly
+
+from main.models import ItemImageModel, ItemModel
+from .serializers import ItemSerializer
+
+
+# Add or post item api view with multiple images
+class ItemListAdd(generics.ListCreateAPIView):
+    """Allow to post item only authenticated user"""
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+    queryset = ItemModel.objects.all()
+    serializer_class = ItemSerializer
+    lookup_field = "itemId"
+
+    def post(self, request):
+        # Passing request of data and the request context for files
+        serializer = ItemSerializer(data=request.data, context={"request": request})
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class ItemRUD(generics.RetrieveUpdateDestroyAPIView):
+    """View that can handle item get, update and delete"""
+
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    serializer_class = ItemSerializer
+    queryset = ItemModel.objects.all()
+    lookup_field = "itemId"
+
+    def get(self, request, itemId=None):
+        return self.retrieve(request, itemId)
+
+    def put(self, request, itemId=None):
+        return self.update(request, itemId)
+
+    def delete(self, request, itemId=None):
+        return self.destroy(request, itemId)  # send custom deletion success message
--- src/urls.py
+++ src/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.views.static import serve
+from django.conf import settings
+from django.conf.urls import url
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -29,4 +32,6 @@ urlpatterns = [
     ),
     path('admin/', admin.site.urls),
     path('api/v1/', include('user.urls')),
+    path('api/v1/', include('post.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,})
 ]
