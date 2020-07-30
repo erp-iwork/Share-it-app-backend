d10888b3d16c0b77f07f863beff515bc3b094956
endalk <endalkhbel@gmail.com>
2020-07-30 10:52:21 +0300

    make it better

--- src/settings.py
+++ src/settings.py
@@ -183,7 +183,8 @@ CHANNEL_LAYERS = {
     "default": {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [("redis", 6379)],
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            # "hosts": [("redis", 6379)],
         },
     },
 }
