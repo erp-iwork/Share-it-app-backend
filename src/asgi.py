f895586659c6bc6e98632ad463db5e04177c0ee7
endalk <endalkhbel@gmail.com>
2020-07-30 10:09:43 +0300

    make it better

--- src/asgi.py
+++ src/asgi.py
@@ -1,11 +1,5 @@
-"""
-ASGI entrypoint. Configures Django and then runs the application
-defined in the ASGI_APPLICATION setting.
-"""
-
 import os
-import django
-from channels.routing import get_default_application
+import channels.asgi
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.settings")
-django.setup()
-application = get_default_application()
+channel_layer = channels.asgi.get_channel_layer()
